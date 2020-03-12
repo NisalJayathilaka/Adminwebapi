9d97e5827e88431e160f6ec36200c16fac47b081
Nayana <nayana1995@icloud.com>
2020-03-12 11:35:34 +0530

    deployed product routes
    deploay product routes for handle the frontend api calls with product controller

---
+++ Server/Product/Product.Routes.js
@@ -0,0 +1,79 @@
+var express = require('express');
+var router = express.Router();
+var ProductController = require('./Product.Controller');
+const multer=require('multer');
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, './uploads/');
+    },
+    filename: function(req, file, cb) {
+      cb(null, new Date().toISOString() + file.originalname);
+    }
+  });
+
+const fileFilter = (req, file, cb) => {
+    // reject a file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+      cb(null, true);
+    } else {
+        cb(new Error('invalid file format'), false);
+        return
+    }
+  };
+
+  const upload = multer({
+    storage: storage,
+    limits: {
+      fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+  });
+
+router.post("/", upload.single('ImgPath'),(req,res)=>{
+    console.log(req.file);
+    ProductController.Insert(req).then((data)=>{
+        res.status(data.status).send(data.message);
+    }).catch((err)=>{
+        res.status(err.status).send(err.message);
+    });
+});
+
+router.get('/',(req,res)=>{
+    let query = req.params.query;
+    ProductController.retrieve(query).then((data)=>{
+        res.status(data.status).send(data.message);
+    }).catch((err)=>{
+        res.status(err.status).send(err.message);
+    });
+});
+
+router.get('/:id',(req,res)=>{
+    let id = req.params.id;
+    ProductController.retrieveByID(id).then((data)=>{
+        res.status(data.status).send(data.message);
+    }).catch((err)=>{
+        res.status(err.status).send(err.message);
+    });
+});
+
+router.put('/:id',(req,res)=>{
+    let id = req.params.id;
+    ProductController.update(id,req.body).then((data)=>{
+        res.status(data.status).send(data.message);
+    }).catch((err)=>{
+        res.status(err.status).send(err.message);
+    });
+});
+
+router.delete('/:id',(req,res)=>{
+    let id = req.params.id;
+    ProductController.delete(id).then((data)=>{
+        res.status(data.status).send(data.message);
+    }).catch((err)=>{
+        res.status(err.status).send(err.message);
+    });
+});
+
+
+
+module.exports = router;
